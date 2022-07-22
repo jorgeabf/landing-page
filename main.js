5b759e0012f6bea07e30bb7ef6924ec91438b156
Jorge <aportuvas@outlook.es>
2022-07-22 04:12:24 +0200

    Arreglados detalles maquetación

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ function abrirMenu() {
   menuMovil.classList.toggle("menuVisible");
 }
 
+menuMovil.addEventListener("click", (e) => {
+  console.log(e.target.className);
+  if (e.target.className.includes("")) {
+    menuMovil.classList.remove("menuVisible");
+  }
+});
+
 ScrollReveal().reveal(".showcase");
-ScrollReveal().reveal(".news-cards", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
+ScrollReveal().reveal(".news-cards", { delay: 200 });
+ScrollReveal().reveal(".cards-banner-one", { delay: 200 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 200 });
